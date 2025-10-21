149a9fc7e5c193b2572151ba789884cfabe9f92f
km222uq <km222uq@student.lnu.se>
2025-10-21 23:45:12 +0200

    sha256 hotfix

--- Formula/songfetch.rb
+++ Formula/songfetch.rb
@@ -2,7 +2,7 @@ class Songfetch < Formula
   desc "A CLI tool that displays current song information in the terminal"
   homepage "https://github.com/fwtwoo/songfetch"
   url "https://github.com/fwtwoo/songfetch/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "71ba5a2649c59caa9bfb12ceb8077857241aed25a40631d78acbe634aa28f144"
+  sha256 "1b4c73283d7b5981b314ae3f77b6150947077aa505ab8869bb12bce0df6f7bf4"
   license "GPL-2.0"
 
   depends_on "python@3.14"
